bc950f2c4bf7544cd9f736a01d4883424a0a1075
kaizer113 <lionel.giavelli@gmail.com>
2022-08-24 14:54:10 -0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name          TestScript
+// @description   This is your new file, start writing code
+// @match         <all_urls>
+// @exclude-match *://*.google.com/*
+// @noframes
+// ==/UserScript==
+
+alert("This is a test script");
